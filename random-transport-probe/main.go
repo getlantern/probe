fe3b4ae4efa4fd89469b85110c595ade187bb677
Harry Harpham <harry@getlantern.org>
2019-07-18 17:08:07 -0500

    Update utility

--- random-transport-probe/main.go
+++ random-transport-probe/main.go
@@ -4,54 +4,43 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"time"
 
 	"github.com/getlantern/probe"
 )
 
-// baseline response:
+// Using baseline:
 // 	min response time: 37.609275s
 // 	max response time: 2m15.187502s
 // 	response time standard deviation: 25.832489579s
 
 var (
-	addr = flag.String("addr", "52.231.39.25:443", "address to hit")
+	addr     = flag.String("addr", "52.231.39.25:443", "address to hit")
+	baseline = flag.String("baseline", "randomized-transport.baseline", "baseline file")
 )
 
 func main() {
+	flag.Parse()
 
-	// debugging
-	min, err := time.ParseDuration("1m30.190914s")
+	f, err := os.Open(*baseline)
 	if err != nil {
-		panic(err)
+		fmt.Println("failed to open baseline file:", err)
+		os.Exit(1)
 	}
-	max, err := time.ParseDuration("2m15.263066s")
-	if err != nil {
-		panic(err)
-	}
-	stddev, err := time.ParseDuration("22.042394335s")
-	if err != nil {
-		panic(err)
-	}
-	err = probe.RTShortcut(probe.Config{
-		Network: "tcp",
-		Address: *addr,
-	}, float64(min), float64(max), float64(stddev))
+	defer f.Close()
 
-	// err := probe.ForRandomizedTransport(probe.Config{
-	// 	Network: "tcp",
-	// 	Address: *addr,
-	// })
-	if err == nil {
-		fmt.Printf("server at %s does not appear to be a randomized transport\n", *addr)
-		os.Exit(0)
-	}
-	switch err.(type) {
-	case probe.FailedCheck:
-		fmt.Println(err)
-		os.Exit(2)
-	default:
+	results, err := probe.ForRandomizedTransport(probe.Config{
+		Network:      "tcp",
+		Address:      *addr,
+		Logger:       os.Stdout,
+		BaselineData: f,
+	})
+	if err != nil {
 		fmt.Println("error running test:", err)
 		os.Exit(1)
 	}
+	if results.Success {
+		fmt.Println(results.Explanation)
+		os.Exit(2)
+	}
+	fmt.Printf("server at %s does not appear to be a randomized transport\n", *addr)
 }
