cdb699e40899b7d147127865f45e8d1048b020e5
Harry Harpham <harry@getlantern.org>
2019-07-24 18:22:59 -0500

    Update utility

--- random-transport-probe/main.go
+++ random-transport-probe/main.go
@@ -3,41 +3,56 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"os"
 
 	"github.com/getlantern/probe"
 )
 
-// Using baseline:
-// 	min response time: 37.609275s
-// 	max response time: 2m15.187502s
-// 	response time standard deviation: 25.832489579s
-
 var (
-	addr     = flag.String("addr", "52.231.39.25:443", "address to hit")
-	baseline = flag.String("baseline", "randomized-transport.baseline", "baseline file")
+	addr          = flag.String("addr", "52.231.39.25:443", "address to hit")
+	baseline      = flag.String("baseline", "", "baseline file")
+	writeBaseline = flag.Bool("write-baseline", false, "overwrite the baseline file")
 )
 
 func main() {
 	flag.Parse()
 
-	f, err := os.Open(*baseline)
-	if err != nil {
-		fmt.Println("failed to open baseline file:", err)
-		os.Exit(1)
+	cfg := probe.Config{
+		Network:        "tcp",
+		Address:        *addr,
+		Logger:         os.Stderr,
+		MaxParallelism: 100,
 	}
-	defer f.Close()
 
-	results, err := probe.ForRandomizedTransport(probe.Config{
-		Network:      "tcp",
-		Address:      *addr,
-		Logger:       os.Stdout,
-		BaselineData: f,
-	})
+	if *baseline != "" && !(*writeBaseline) {
+		f, err := os.Open(*baseline)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "failed to open baseline file:", err)
+			os.Exit(1)
+		}
+		defer f.Close()
+		cfg.BaselineData = f
+	}
+
+	results, err := probe.ForRandomizedTransport(cfg)
 	if err != nil {
-		fmt.Println("error running test:", err)
+		fmt.Fprintln(os.Stderr, "error running test:", err)
 		os.Exit(1)
 	}
+
+	if *baseline != "" && *writeBaseline {
+		f, err := os.Create(*baseline)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "failed to open baseline file:", err)
+			os.Exit(1)
+		}
+		defer f.Close()
+		if _, err := io.Copy(f, results.BaselineData); err != nil {
+			fmt.Fprintln(os.Stderr, "failed to write baseline data:", err)
+		}
+	}
+
 	if results.Success {
 		fmt.Println(results.Explanation)
 		os.Exit(2)
